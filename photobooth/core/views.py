15c40c9281494f049a1cd0834fd6ec8db9bf434f
DogNellaf <dans-host@ya.ru>
2024-02-22 22:01:01 +0300

    Index and snap pages

--- photobooth/core/views.py
+++ photobooth/core/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, "index.html")
+
+def snap(request):
+    return render(request, "snap.html")
