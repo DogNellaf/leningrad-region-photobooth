0edd797bf6315093a5ea1ea43fe0a95115bd6f34
dognellaf <dans-host@ya.ru>
2024-02-24 02:59:37 +0300

    fix

--- photobooth/core/views.py
+++ photobooth/core/views.py
@@ -6,7 +6,7 @@ from django.views.decorators.csrf import csrf_exempt
 import os
 import base64
 
-STATIC_DIR = STATICFILES_DIRS[0]
+STATIC_DIR = STATICFILES_DIRS[1]
 
 def index(request):
     return render(request, "index.html")
@@ -40,32 +40,6 @@ def save_snap(request):
 
         with open(path, 'wb') as f: 
             f.write(data)
-
-        # # input_path = 'input.png'
-        # # output_path = 'output.png'
-        # # Загрузка изображения
-        # image = cv2.imread(path)
-        # # Преобразование изображения в оттенки серого
-        # gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-        # # Применение порогового преобразования для выделения объекта
-        # _, thresh = cv2.threshold(gray, 240, 255, cv2.THRESH_BINARY)
-
-        # # Нахождение контуров на пороговом изображении
-        # contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-        # # Создание маски для объекта
-        # mask = np.zeros_like(image)
-        # cv2.drawContours(mask, contours, -1, (255, 255, 255), thickness=cv2.FILLED)
-
-        # # Инвертирование маски
-        # mask_inv = cv2.bitwise_not(mask)
-
-        # # Применение маски к исходному изображению для удаления фона
-        # result = cv2.bitwise_and(image, image, mask=mask_inv)
-
-        # # Сохранение результата
-        # cv2.imwrite(path, result)
 
         return JsonResponse({'success': True, 'image_name': image_name})
     return JsonResponse({'success': False, 'image_name': None})
