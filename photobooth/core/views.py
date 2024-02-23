a862ece76d199e320b8f6254a85c990fb74f3b35
DogNellaf <dans-host@ya.ru>
2024-02-23 12:34:05 +0300

    Rewrite templates and some fixes

--- photobooth/core/views.py
+++ photobooth/core/views.py
@@ -6,3 +6,9 @@ def index(request):
 
 def snap(request):
     return render(request, "snap.html")
+
+def location(request):
+    return render(request, "location.html")
+
+def result(request):
+    return render(request, "result.html")
--- photobooth/photobooth/urls.py
+++ photobooth/photobooth/urls.py
@@ -4,5 +4,7 @@ from core import views
 urlpatterns = [
     #path('admin/', admin.site.urls),
     path('', views.index, name='index'),
-    path('snap', views.snap, name='snap')
+    path('snap', views.snap, name='snap'),
+    path('location', views.location, name='location'),
+    path('result', views.result, name='result')
 ]
