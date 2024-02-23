3de0df31abe1df49af2e6eb051a251aeb062c89e
DogNellaf <dans-host@ya.ru>
2024-02-23 21:58:29 +0300

    fix

--- photobooth/core/views.py
+++ photobooth/core/views.py
@@ -15,14 +15,16 @@ def snap(request):
     return render(request, "snap.html")
 
 def location(request, snap_url):
-    return render(request, "location.html", {'snap_url': os.path.join("snaps", snap_url)})
+    return render(request, "location.html", {'snap_url': snap_url})
 
 def editor(request, snap_url, location_title):
-    location_title = 'location/' + location_title
+    location_title = os.path.join("location", location_title)
+    photo_url = os.path.join("snaps", photo_url)
     return render(request, "editor.html", {'location_title': location_title, 'snap_url': snap_url})
 
 def result(request, photo_url):
-    return render(request, "result.html", {'photo_url': os.path.join("images", photo_url)})
+    photo_url = os.path.join("images", photo_url)
+    return render(request, "result.html", {'photo_url': photo_url})
 
 @csrf_exempt
 def save_snap(request):
