e10c703110d1e8eea7e49d9246f6e596b97f4cfd
dognellaf <dans-host@ya.ru>
2024-02-24 18:48:05 +0300

    fix

--- photobooth/static/result/keyboard.js
+++ photobooth/static/result/keyboard.js
@@ -45,9 +45,9 @@ const Keyboard = {
         const fragment = document.createDocumentFragment();
         const keyLayout = [
             "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "backspace",
-            "q", "w", "e", "r", "t", "y", "u", "i", "o", "p",
+            "q", "w", "e", "r", "t", "y", "u", "i", "o", ".", "p",
             "caps", "a", "s", "d", "f", "g", "h", "j", "k", "l",
-            "done", "z", "x", "c", "v", "b", "n", "m", ",", ".", "@"
+            "done", "z", "x", "c", "v", "b", "n", "m", "_", "-", "@"
         ];
 
         // Creates HTML for an icon
