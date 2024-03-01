f22e49290e81f877d2813345b486c6ff40024393
DogNellaf <dans-host@ya.ru>
2024-03-01 13:30:29 +0300

    run.bat fix and timer center align

--- run.bat
+++ run.bat
@@ -1,8 +1,8 @@
 @echo off
 IF NOT EXIST venv (
-    py -m venv venv
+    py -m venv .venv
 )
-call venv\Scripts\activate.bat
+call .venv\Scripts\activate.bat
 pip install -r requirements.txt
 cd photobooth
 python manage.py runserver
